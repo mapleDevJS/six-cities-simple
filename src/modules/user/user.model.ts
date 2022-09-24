a2bdc949db4a92518d6150b83df56b28b16ade9a
Alexey Ivanov <alexey.ivanov@globalrelay.net>
2022-09-24 14:55:03 -0700

    module4-task1: add integrated validation

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -10,9 +10,16 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     unique: true,
+    match: [/^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Email is incorrect'],
+    required: true,
   },
   avatarPath: String,
-  firstname: String,
+  firstname: {
+    type: String,
+    required: true,
+    minlength: [1, 'Min length for firstname is 1'],
+    maxlength: [15, 'Max length for firstname is 15'],
+  },
   lastname: String,
 }, {
   timestamps: true,
