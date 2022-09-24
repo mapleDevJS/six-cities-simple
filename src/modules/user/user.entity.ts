630356c7716ddd1a2c1b5299576286cc3787c426
Alexey Ivanov <alexey.ivanov@globalrelay.net>
2022-09-24 15:02:53 -0700

    module4-task1: wip: add user schema description

--- src/modules/user/user.entity.ts
+++ src/modules/user/user.entity.ts
@@ -1,8 +1,26 @@
 import {User} from '../../types/user.type.js';
+import typegoose, {getModelForClass} from '@typegoose/typegoose';
+
+const {prop} = typegoose;
 
 export class UserEntity implements User {
+  @prop({ unique: true, required: true })
   public email!: string;
+
+  @prop()
   public avatarPath!: string;
+
+  @prop({ required: true, minlength: 1, maxlength: 15 })
   public firstname!: string;
+
+  @prop()
   public lastname!: string;
+
+  @prop({ required: true, minlength: 6, maxlength: 12 })
+  public password!: string;
+
+  @prop()
+  public userType!: string;
 }
+
+export const UserModel = getModelForClass(UserEntity);
