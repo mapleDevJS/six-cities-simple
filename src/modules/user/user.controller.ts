c972de22b3ab60bd44afbe72edf2f164301f4e1c
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2022-10-09 16:52:37 -0700

    module6-task1: add user creating to UserController

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,15 +1,23 @@
-import {NextFunction, Request, Response} from 'express';
+import {Request, Response} from 'express';
 import {inject, injectable} from 'inversify';
 import {Controller} from '../../common/controller/controller.js';
 import {Component} from '../../types/component.types.js';
 import {LoggerInterface} from '../../common/logger/logger.interface.js';
 import {HttpMethod} from '../../types/http-method.enum.js';
 import CreateUserDto from './dto/create-user.dto.js';
+import {UserServiceInterface} from './user-service.interface.js';
+import HttpError from '../../common/errors/http-error.js';
+import {StatusCodes} from 'http-status-codes';
+import {fillDTO} from '../../utils/common.js';
+import UserResponse from './response/user.response.js';
+import {ConfigInterface} from '../../common/config/config.interface.js';
 
 @injectable()
 export default class UserController extends Controller {
   constructor(
     @inject(Component.LoggerInterface) logger: LoggerInterface,
+    @inject(Component.UserServiceInterface) private readonly userService: UserServiceInterface,
+    @inject(Component.ConfigInterface) private readonly configService: ConfigInterface,
   ) {
     super(logger);
     this.logger.info('Register routes for UserController…');
@@ -18,10 +26,23 @@ export default class UserController extends Controller {
   }
 
   public async create(
-    _req: Request<Record<string, unknown>, Record<string, unknown>, CreateUserDto>,
-    _res: Response,
-    _next: NextFunction
+    {body}: Request<Record<string, unknown>, Record<string, unknown>, CreateUserDto>,
+    res: Response,
   ): Promise<void> {
-    throw new Error('[UserController] Oops');
+    const existsUser = await this.userService.findByEmail(body.email);
+    if (existsUser) {
+      throw new HttpError(
+        StatusCodes.CONFLICT,
+        `User with email «${body.email}» exists.`,
+        'UserController'
+      );
+    }
+
+    const result = await this.userService.create(body, this.configService.get('SALT'));
+    this.send(
+      res,
+      StatusCodes.CREATED,
+      fillDTO(UserResponse, result)
+    );
   }
 }
