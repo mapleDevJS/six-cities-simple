6e2edd2d184204021c0ba87a82c1125fc0bcf873
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2022-10-09 16:44:00 -0700

    module6-task1: [wip] wrapp error into next

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,9 +1,9 @@
-import {Controller} from '../../common/controller/controller.js';
+import {NextFunction, Request, Response} from 'express';
 import {inject, injectable} from 'inversify';
+import {Controller} from '../../common/controller/controller.js';
 import {Component} from '../../types/component.types.js';
 import {LoggerInterface} from '../../common/logger/logger.interface.js';
 import {HttpMethod} from '../../types/http-method.enum.js';
-import {Request, Response} from 'express';
 import CreateUserDto from './dto/create-user.dto.js';
 
 @injectable()
@@ -19,8 +19,13 @@ export default class UserController extends Controller {
 
   public async create(
     _req: Request<Record<string, unknown>, Record<string, unknown>, CreateUserDto>,
-    _res: Response
+    _res: Response,
+    next: NextFunction
   ): Promise<void> {
-    throw new Error('[UserController] Oops');
+    try {
+      throw new Error('[UserController] Oops');
+    } catch (error) {
+      return next(error);
+    }
   }
 }
