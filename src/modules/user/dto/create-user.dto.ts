fea5a3a24497b5da53338353dc913667736e94dd
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2022-10-12 19:21:37 -0700

    module7-task1: add validation for CreateUserDto

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,8 +1,24 @@
+import {IsBoolean, IsEmail, IsString, Length} from 'class-validator';
+
 export default class CreateUserDto {
+  @IsEmail({}, {message: 'email must be valid address'})
   public email!: string ;
+
+  @IsString({message: 'avatarPath is required'})
   public avatarPath!: string;
+
+  @IsString({message: 'firstname is required'})
+  @Length(1, 15, {message: 'Min length is 1, max is 15'})
   public firstName!: string;
+
+  @IsString({message: 'lastname is required'})
+  @Length(1, 15, {message: 'Min length is 1, max is 15'})
   public lastName!: string;
-  isPro!: boolean;
+
+  @IsBoolean({message: 'isPro must be true or false'})
+  public isPro!: boolean;
+
+  @IsString({message: 'password is required'})
+  @Length(6, 12, {message: 'Min length for password is 6, max is 12'})
   public password!: string;
 }
