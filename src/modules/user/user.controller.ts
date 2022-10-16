a87b5502ee90578f7a1d5687b1d8405b4ee6da80
Alexey Ivanov <alexey.ivanov.private@gmail.com>
2022-10-15 18:59:57 -0700

    module8-task1: add authorization check

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -50,6 +50,12 @@ export default class UserController extends Controller {
         new UploadFileMiddleware(this.configService.get('UPLOAD_DIRECTORY'), 'avatar'),
       ]
     });
+
+    this.addRoute({
+      path: '/login',
+      method: HttpMethod.Get,
+      handler: this.checkAuthenticate
+    });
   }
 
   // Создание нового пользователя.
@@ -102,5 +108,11 @@ export default class UserController extends Controller {
     this.created(res, {
       filepath: req.file?.path
     });
+  }
+
+  public async checkAuthenticate(req: Request, res: Response) {
+    const user = await this.userService.findByEmail(req.user.email);
+
+    this.ok(res, fillDTO(LoggedUserResponse, user));
   }
 }
